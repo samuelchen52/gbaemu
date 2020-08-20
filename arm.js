1493aae3756ec1a41fb24a003ea27e966e7b1585
samuel <samuechen@cs.stonybrook.edu>
2020-08-20 02:10:42 -0400

    decode second group of instructions

--- arm.js
+++ arm.js
@@ -14,7 +14,7 @@ const arm = function(memory, registers) {
 			//0tt1 -> use register after shifting with bottom byte of another register
 			//mmmm mmmm -> use 8 bit imm after 4 bit imm shift
 
-			switch (bitSlice(instr, 24, 27)) //bits 25 to 27
+			switch (bitSlice(instr, 24, 27))
 			{
 				case 0: //FIRST GROUP OF INSTRUCTIONS
 				if (bitSlice(instr, 4, 4))
@@ -110,11 +110,134 @@ const arm = function(memory, registers) {
 
 						case 7: break;	//RSC stt0 
 					}
-					throw Error("encountered undefined instruction!");
 				}
 				break;
-
+				//3322 2222 2222 1111 1111 1100 0000 0000
+				//1098 7654 3210 9876 5432 1098 7654 3210
+				//xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx
 				case 1:
+				if (bitSlice(instr, 4, 4))
+				{
+					if (bitSlice(instr, 7, 7))
+					{
+						switch (bitSlice(instr, 21, 23))
+						{
+							case 0: break;	//TST 0tt1 check if S bit has been set to 1
+
+							case 1: 
+							if (bitSlice(instr, 20, 20)) //S bit differentiates TEQ from BRANCH AND EXCHANGE with this parsing
+							{
+								//TEQ
+							}
+							else
+							{
+								//BRANCH AND EXCHANGE check if a whole bunch of bits are set
+							}
+							break;	//TEQ 0tt1 check if S bit has been set to 1 
+
+							case 2: break;	//CMP 0tt1 check if S bit has been set to 1
+
+							case 3: break;	//CMN 0tt1 check if S bit has been set to 1
+
+							case 4: break;	//ORR 0tt1 
+
+							case 5: break;	//MOV 0tt1 check if some bits are set to zero
+
+							case 6: break;	//BIC 0tt1 
+
+							case 7: break;	//MVN 0tt1 check if some bits are set to zero
+						}
+					}
+					else
+					{
+						switch (bitSlice(instr, 5, 6))
+						{
+							case 0:
+							break;	//SWP check if a bunch of bits are zero
+
+							case 1:
+							switch ((bitSlice(instr, 20, 20) << 1) + bitSlice(instr, 23, 23))
+							{
+								case 0:
+								break;	//STRH p=1 i=0 check if bits are zero 
+
+								case 1:
+								break;	//LDRH p=1 i=0 check if bits are zero
+
+								case 2:
+								break;	//STRH p=1 i=1
+
+								case 3:
+								break;	//LDRH p=1 i=1
+							}
+							break;
+
+							case 2:
+							switch ((bitSlice(instr, 20, 20) << 1) + bitSlice(instr, 23, 23))
+							{
+								case 1:
+								break;	//LDRSB p=1 i=0	check if bits are zero
+
+								case 3:
+								break;	//LDRSB p=1 i=1
+							}
+							break;
+
+							case 3:
+							switch ((bitSlice(instr, 20, 20) << 1) + bitSlice(instr, 23, 23))
+							{
+								case 1:
+								break;	//LDRSH p=1 i=0	check if bits are zero
+
+								case 3:
+								break;	//LDRSH p=1 i=1
+							}
+							break;
+						}
+					}
+				}
+				else
+				{
+					switch(bitSlice(instr, 20, 23))
+					{
+						case 0:
+						case 4:
+						break;	//MRS check if a whole bunch of bits are set
+						
+						case 2:
+						case 6:
+						break;	//MRS register check if a whole bunch of bits are set
+
+						case 1:
+						break;	//TST stt0
+
+						case 3:
+						break;	//TEQ stt0
+
+						case 5:
+						break;	//CMP stt0
+
+						case 7:
+						break;	//CMN stt0
+
+						case 8:
+						case 9:
+						break;	//ORR stt0
+
+						case 10:
+						case 11:
+						break;	//MOV stt0 check if some bits are set to zero
+
+						case 12:
+						case 13:
+						break;	//BIC stt0
+
+						case 14:
+						case 15:
+						break;	//MVN stt0 check if some bits are set to zero
+
+					}
+				}
 				break;
 
 				case 2:
