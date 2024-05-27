0f90898bea063ad559b179f1487b7e4f8aa3acaa
Samuel Chen <samuelchen52@gmail.com>
2024-05-26 22:14:10 -0400

    channel.js / sound.js - cleanup

--- src/sound/channel.js
+++ src/sound/channel.js
@@ -15,16 +15,6 @@ const squareChannel = function(REG_SOUNDCNT_LEN, REG_SOUNDCNT_FREQ, REG_SOUNDCNT
     this.sweepMode; //bool, if true, freq will increase
     this.sweepTime; //3 bit number that dictates delay is between each sweep
 
-    //this.cycle = 0;
-    //this.cyclesSinceSweep; //based on sweepTime
-    
-    //this.wavePatternCycle = 0; //
-
-    this.enabled = false;
-
-    //gba sampling rate is 32.768kHz, 16.78 mhz means around 500 cycles per sample.
-    //this.cyclesPerPeriod; //this refers to cycles for a full wave period, depends on freq
-
     this.cyclesPerSweepStep = 0; //change freq every cyclesPerSweepStep
     this.cyclesPerEnvelopeStep = 0; //change volume every cyclesPerEnvelopeStep
 
@@ -49,93 +39,11 @@ const squareChannel = function(REG_SOUNDCNT_LEN, REG_SOUNDCNT_FREQ, REG_SOUNDCNT
         [1,1,1,1,1,1,0,0],
     ]
 
-    //stopping conditions
-    //sweep direction set to ADD, when set to decrease goes on as normal???
-    //timed mode is set, and sound length is reached
-
-    //starting conditions
-    //sound reset is toggled on, need to establish when reset is toggled if timed mode is enabled
-
-    //mute
-    //e.g. if disabled, do the logic, but return 0
-    //external volume 
-    //master sound control
-
-    //continouous (always do)
-    //square wave
-
-    //continuouse after starting (do after starting) i.e. need to maintain 
-    //everything else
-
-    //all events
-    //every interval, wave change
-    //sweep change (because steptime can change dynamically, will have to check on lowest interval)
-    //volume change (because steptime can change dynamically, will have to check on lowest interval)
-    //getSound() can get called at anytime, will retrieve sound based on current state of channel (should be called last prob)
-
-    //start with initial frequency
-    //check frequency change every 1 / 128 khz (call this steps). if this matches with the current flag, then do the sweep change. e.g. if sweep delay is every 3 steps. check if we are on the third step, then initiate freq change
-
-    //start with initial volume
-    //do the same with the envelope
-
-    //for timed mode, set a boolean that indicates whether or not the sound is timed, when the channel wakes up!!!
-    //follow the stopping conditions above
-    
-    //duty cycle??
-    //as long as we know where we are relative to the beginning of the square wave cycle, then we know whether or not to emit a sound
-    //duty cycle depends on frequency. on frequency change,  drain the cycles
-
-
-
     REG_SOUNDCNT_LEN.addCallback((REGSOUNDCNTLENVal) => this.updateREGSOUNDCNTLEN(REGSOUNDCNTLENVal) );
     REG_SOUNDCNT_FREQ.addCallback((REGSOUNDCNTFREQVal) => this.updateREGSOUNDCNTFREQ(REGSOUNDCNTFREQVal) );
     if (REG_SOUNDCNT_SWEEP)
         REG_SOUNDCNT_SWEEP.addCallback((REGSOUNDCNTSWEEPVal) => this.updateREGSOUNDCNTSWEEP(REGSOUNDCNTSWEEPVal) );
-
-    //optional
-
-    //SOUND1CNT_H
-
-    // // Define PCM data (this is just an example, replace it with your actual PCM data)
-    // const pcmData = generateSineWave(samples, frequency, 32768);
-
-    // // Create an AudioBuffer to hold the PCM data
-    // const audioBuffer = audioContext.createBuffer(1, pcmData.length, 32768);
-
-    // // Fill the AudioBuffer with the PCM data
-    // const channelData = audioBuffer.getChannelData(0);
-    // for (let i = 0; i < pcmData.length; i++) {
-    //     channelData[i] = pcmData[i];
-    // }
-
-    // // Create an AudioBufferSourceNode
-    // const source = audioContext.createBufferSource();
-    // source.buffer = audioBuffer;
-
-    // // Connect the AudioBufferSourceNode to the AudioContext's destination (e.g., speakers)
-    // source.connect(audioContext.destination);
-
-    // // Start playing the audio
-    // source.start();
-    
-    //sound channels 1 - 6
 };
-
-	// //REG_SOUNDCNT_SWEEP
-	// REG_SOUNDCNT_SWEEP_SHIFT: convertStringToBitMask("111"),
-	// REG_SOUNDCNT_SWEEP_INCREASE: convertStringToBitMask("1000"),
-	// REG_SOUNDCNT_SWEEP_SWEEP_TIME: convertStringToBitMask("1110000"),
-	// //REG_SOUNDCNT_LEN
-	// REG_SOUNDCNT_LEN_LENGTH: convertStringToBitMask("111111"),
-	// REG_SOUNDCNT_LEN_WAVE_DUTY: convertStringToBitMask("11000000"),
-	// REG_SOUNDCNT_LEN_ENV_STEP_TIME: convertStringToBitMask("11100000000"),
-	// REG_SOUNDCNT_LEN_ENV_MODE: convertStringToBitMask("100000000000"),
-	// REG_SOUNDCNT_LEN_ENV_INIT: convertStringToBitMask("1111000000000000"),
-	// //REG_SOUNDCNT_FREQ
-	// REG_SOUNDCNT_FREQ_SOUND_FREQ: convertStringToBitMask("11111111111"),
-	// REG_SOUNDCNT_FREQ_TIMED_MODE: convertStringToBitMask("100000000000000"),
-	// REG_SOUNDCNT_FREQ_SOUND_RESET: convertStringToBitMask("1000000000000000"),
 
 squareChannel.prototype.updateREGSOUNDCNTLEN = function(REGSOUNDCNTLENVal) {
     this.soundLength = (REGSOUNDCNTLENVal & IORegisterMasks["REG_SOUNDCNT_LEN_LENGTH"]) >>> IORegisterMaskShifts["REG_SOUNDCNT_LEN_LENGTH"];
@@ -178,16 +86,9 @@ squareChannel.prototype.updateREGSOUNDCNTSWEEP = function(REGSOUNDCNTSWEEPVal) {
 //if state hasn't changed between each sample rate, which is 52k, then the sound generated won't be changed
 //hopefully, it comes out fine (seemed to work out fine for timers, which are also dynamic :/ ?)
 squareChannel.prototype.update = function(numCycles) {
-    //if (!this.enabled)
-        //return Number.MAX_SAFE_INTEGER; //return some arbitrarily large number for the scheduler
-    if (!this.enabled)
-        return;
-
     this.updateVolume(numCycles);
     this.updateFrequency(numCycles);
     this.updateDutyCycle(numCycles);
-    
-
 };
 
 squareChannel.prototype.updateVolume = function(numCycles) {
@@ -214,9 +115,6 @@ squareChannel.prototype.updateFrequency = function(numCycles) {
     for (let i = 0; i < Math.floor(this.cyclesSinceFrequencyChange / this.cyclesPerSweepStep); i ++) {
         let tempWavePeriodInSeconds = this.wavePeriodInSeconds + (this.wavePeriodInSeconds / Math.pow(2, this.sweepShifts) * (this.sweepMode ? 1 : -1));
         let tempFrequency = Math.floor(1 / tempWavePeriodInSeconds);
-        //this.wavePeriodInSeconds += this.wavePeriodInSeconds / Math.pow(2, this.sweepShifts) * (this.sweepMode ? 1 : -1);
-        //this.wavePeriodInCycles = Math.floor(this.wavePeriodInSeconds * CYCLES_PER_SECOND);
-        //this.frequency = Math.floor(1 / this.wavePeriodInSeconds); //wavePeriodInSeconds >= 1,  
 
         if (tempFrequency <= 64)
             break;
@@ -243,13 +141,6 @@ squareChannel.prototype.updateDutyCycle = function(numCycles) {
     }
 
     this.wavePeriodPos = Math.floor(this.wavePeriodCycle / this.wavePeriodInCycles / .125);
-
-
-    //this.wavePeriodInCycles = Math.floor(this.wavePeriodInSeconds * CYCLES_PER_SECOND);
-    //this.wavePeriodPos %= 8;
-
-    // if (isNaN(this.wavePeriodPos) || isNaN(this.wavePeriodCycle))
-    //     4 + 3;
 };
 
 squareChannel.prototype.updateDuration = function(numCycles) {
@@ -259,20 +150,15 @@ squareChannel.prototype.updateDuration = function(numCycles) {
 
 squareChannel.prototype.getSample = function() {
     
-    if (this.enabled 
-        && (this.volume > 0) 
+    if ((this.volume > 0) 
         && (this.frequency <= 131072) //stop playing sound if frequency exceeds maximum val
     )
         return (this.wavePatternArr[this.wavePattern][this.wavePeriodPos] * (this.volume / this.maxVolume)); //normalize volume with 1 represent max volume val
-    // this.yes = !this.yes;
-    // return this.yes ? 1 : 0;
 
     return 0;
 };
 
-squareChannel.prototype.start = function() {
-    this.enabled = true
-    
+squareChannel.prototype.init = function() {    
     //init volume
     this.volume = this.initialEnvelopeValue;
     this.cyclesSinceEnvelopeChange = 0;
@@ -295,9 +181,6 @@ squareChannel.prototype.start = function() {
     this.wavePeriodPos = 0;
 };
 
-squareChannel.prototype.stop = function() {
-    this.enabled = false
-}
 
 //channel 3
 const DACChannel = function(mmu) {
--- src/sound/sound.js
+++ src/sound/sound.js
@@ -1,17 +1,7 @@
 
 const sound = function(mmu) {
-
-    window.sound = this;
-
     //sound api stuff
     this.audioContext = new AudioContext();
-    // let audioBuffer = audioContext.createBuffer(1, 32768, 32768);
-    // this.source = audioContext.createBufferSource();
-    // this.source.buffer = audioBuffer;
-    // this.source.connect(audioContext.destination);
-
-    this.numCycle2 = 0;
-
 
     this.numCycle = 0;
     this.cyclesPerSample = 512; //gba runs at 16mm hz, this means around 32k sampling rate
@@ -19,10 +9,9 @@ const sound = function(mmu) {
     this.sampleArrLength = 4096; //32768 * 2
     this.sampleRate = 32768;
     this.sampleArr = new Array(this.sampleArrLength) //audioBuffer.getChannelData(0);
-    this.soundStart;
-    //this.soundLength = (.75 * this.sampleArrLength) / this.sampleRate; // in seconds, this will basically dictate how much time will pass before we drain the sample buffer i.e. latency
+    this.soundStart = 0;
     this.soundLength = this.sampleArrLength / this.sampleRate; // in seconds, this will basically dictate how much time will pass before we drain the sample buffer i.e. latency
-    this.soundFinished = false;
+    this.pctSoundFinishedThreshold = .85;
 
     this.enable = false;
     
@@ -30,37 +19,14 @@ const sound = function(mmu) {
     let soundEnable = ioregion.getIOReg("SOUNDCNT_X");
 
     soundEnable.addCallback((soundEnableVal) => { 
-        this.enable = (soundEnableVal & IORegisterMasks["REG_MASTER_SOUNDSTAT_ENABLE"]) >>> IORegisterMaskShifts["REG_MASTER_SOUNDSTAT_ENABLE"];
+        let enable = (soundEnableVal & IORegisterMasks["REG_MASTER_SOUNDSTAT_ENABLE"]) >>> IORegisterMaskShifts["REG_MASTER_SOUNDSTAT_ENABLE"];
+        if (enable)
         this.start(this.enable); 
     });
 
+    //channels
     this.squareChannel1 = new squareChannel(ioregion.getIOReg("SOUND1CNT_H"), ioregion.getIOReg("SOUND1CNT_X"), ioregion.getIOReg("SOUND1CNT_L"));
     this.squareChannel2 = new squareChannel(ioregion.getIOReg("SOUND2CNT_L"), ioregion.getIOReg("SOUND2CNT_H"));
-
-    // // Define PCM data (this is just an example, replace it with your actual PCM data)
-    // const pcmData = generateSineWave(samples, frequency, 32768);
-
-    // // Create an AudioBuffer to hold the PCM data
-    // const audioBuffer = audioContext.createBuffer(1, pcmData.length, 32768);
-
-    // // Fill the AudioBuffer with the PCM data
-    // const channelData = audioBuffer.getChannelData(0);
-    // for (let i = 0; i < pcmData.length; i++) {
-    //     channelData[i] = pcmData[i];
-    // }
-
-    // // Create an AudioBufferSourceNode
-    // const source = audioContext.createBufferSource();
-    // source.buffer = audioBuffer;
-
-    // // Connect the AudioBufferSourceNode to the AudioContext's destination (e.g., speakers)
-    // source.connect(audioContext.destination);
-
-    // // Start playing the audio
-    // source.start();
-    
-    //sound channels 1 - 6
-
 
     //actively playing
     this.currentSource = {
@@ -82,12 +48,7 @@ sound.prototype.getSample = function() {
     // let sound6 = this.channel1.getSample();
 
     let res = this.mix(sound1, sound2);
-
-    if (isNaN(res))
-        4 + 2;
     return res;
-
-    //return this.mix(sound1, sound2, sound3, sound4, sound5, sound6);
 }
 
 //in seconds
@@ -96,27 +57,7 @@ sound.prototype.soundStartTimeElapsed = function() {
 }
 
 sound.prototype.playSound = function() {
-    //this.numCycle2 = 0;
-    //return;
-    //console.log(this.sampleArr.map(x => "" + x).join("\n"));
-    // let sum = 0;
-    // this.sampleArr.forEach(x => sum += x);
-    // console.log(sum);
-
-    // let someSound = [];
-    // for (let i = 0; i < 32768; i ++)
-    // {
-    //     if (i % 512 < (512 /2))
-    //         someSound.push(-.5);
-    //     else
-    //         someSound.push(.5);
-    // }
-
-    // let expectedNumSamples = ((this.audioContext.currentTime - this.soundStart)) * this.sampleRate;
-    // let actualNumSamples = this.sampleIndex + 1;
-    // let playbackRate = actualNumSamples / expectedNumSamples;
-
-    //get remaining samples
+    //get remaining samples that have yet to be played from the sound buffer being currently being played
     let currentSoundTimeLeft = this.soundLength - this.soundStartTimeElapsed();
     let samplesRemaining = [];
     if (currentSoundTimeLeft > 0) {
@@ -124,191 +65,39 @@ sound.prototype.playSound = function() {
         samplesRemaining = this.currentSampleArr.slice(this.currentSampleArr.length - numSamplesRemaining , this.currentSampleArr.length);
     }
 
-
+    //determine the playback rate, which will either slow down or speed up the next sound being played
+    //i.e. if emulator is running fast then sound will speed up else slow down
     let expectedNumSamples = ((this.soundLength)) * this.sampleRate;
     let actualNumSamples = this.sampleIndex + samplesRemaining.length;
-    let playbackRate;
-    // if (Math.abs(actualNumSamples - expectedNumSamples) > actualNumSamples / 2)
-    //     playbackRate = actualNumSamples / expectedNumSamples;
-    // else
-        playbackRate = actualNumSamples / expectedNumSamples;
-    //playbackRate = 1;
-    //playbackRate = .80;
+    let playbackRate = actualNumSamples / expectedNumSamples;
 
-    //let playbackRate = 1;
-
-    //  if (this.sampleArr.find(x => x > 0)) {
-    //     console.log(this.sampleIndex + 1);
-    // }
-    if (this.sampleArr.find(x => x > 0)) {
-        console.log(samplesRemaining.length);
-
-        console.log("sound begin: " + (this.audioContext.currentTime)); 
-        //console.log("num samples generated: " + (this.sampleIndex + 1));
-        console.log("expected numSamples: " + expectedNumSamples);
-        console.log("actual numSamples: " + actualNumSamples);
-        console.log("playback rate: " + playbackRate);
-        console.log("")
-    }
-
+    //load up new sound buffer with samples (along with the remaining samples above)
     this.currentSampleArr = this.sampleArr.slice(0, this.sampleIndex);
-
-    try {
-
     var buffer = this.audioContext.createBuffer(1, this.sampleIndex + samplesRemaining.length, this.sampleRate);
     var channelData = buffer.getChannelData(0);
-
-        channelData.set(samplesRemaining.concat(this.currentSampleArr));
-    }
-    catch {
-        console.log();
-    }
+    channelData.set(samplesRemaining.concat(this.currentSampleArr));
         
-        
-    //kill the callback so that toggling of soundfinished flag doesnt bleed over (only happens when buffer fills up fully)
-    this.currentSource.onended = null;
+    //stop the currently playing sound
+    //then immediately after, load up the next buffer of samples and play it
     this.currentSource.stop();
-
-    //new AudioContext().createBufferSource().stop
 
     this.currentSource = this.audioContext.createBufferSource();
     this.currentSource.buffer = buffer;
     this.currentSource.connect(this.audioContext.destination);
     this.currentSource.playbackRate.value = playbackRate;
-    //console.log("STARTING");
-    this.currentSource.onended = () => { 
-        //console.log("seconds elapsed: " + (this.audioContext.currentTime - this.soundStart)); 
-        console.log("sound end: " + (this.audioContext.currentTime)); 
-
-        this.soundFinished = true
-    };
     this.currentSource.start();
 
-    this.soundFinished = false;
     this.soundStart = this.audioContext.currentTime;
 
     //reset the sample index cause we drained it
-        this.sampleIndex = 0;
-
-    //source.stop(this.audioContext.currentTime + (this.sampleRate / this.sampleArrLength) / 2);
-
-    // if (this.prevSource)
-    //     this.prevSource.stop();
-    // this.prevSource = source;
-    // source.
-
+    this.sampleIndex = 0;
 }
-
-// sound.prototype.playSound2 = function() {
-//     //this.numCycle2 = 0;
-//     //return;
-//     //console.log(this.sampleArr.map(x => "" + x).join("\n"));
-//     // let sum = 0;
-//     // this.sampleArr.forEach(x => sum += x);
-//     // console.log(sum);
-
-//     // let someSound = [];
-//     // for (let i = 0; i < 32768; i ++)
-//     // {
-//     //     if (i % 512 < (512 /2))
-//     //         someSound.push(-.5);
-//     //     else
-//     //         someSound.push(.5);
-//     // }
-
-//     // let expectedNumSamples = ((this.audioContext.currentTime - this.soundStart)) * this.sampleRate;
-//     // let actualNumSamples = this.sampleIndex + 1;
-//     // let playbackRate = actualNumSamples / expectedNumSamples;
-
-//     let expectedNumSamples = ((this.soundLength)) * this.sampleRate;
-//     let actualNumSamples = this.sampleIndex;
-//     let playbackRate;
-//     // if (Math.abs(actualNumSamples - expectedNumSamples) > actualNumSamples / 2)
-//     //     playbackRate = actualNumSamples / expectedNumSamples;
-//     // else
-//         playbackRate = actualNumSamples / expectedNumSamples;
-
-//     //let playbackRate = 1;
-
-//     //  if (this.sampleArr.find(x => x > 0)) {
-//     //     console.log(this.sampleIndex + 1);
-//     // }
-//     if (this.sampleArr.find(x => x > 0)) {
-//         console.log("sound begin: " + (this.audioContext.currentTime)); 
-//         //console.log("num samples generated: " + (this.sampleIndex + 1));
-//         console.log("expected numSamples: " + expectedNumSamples);
-//         console.log("actual numSamples: " + actualNumSamples);
-//         console.log("playback rate: " + playbackRate);
-//         console.log("")
-//     }
-
-//     var buffer = this.audioContext.createBuffer(1, this.sampleIndex, this.sampleRate);
-//     var channelData = buffer.getChannelData(0);
-//     channelData.set(this.sampleArr.slice(0, this.sampleIndex));
-
-//     //kill the callback so that toggling of soundfinished flag doesnt bleed over (only happens when buffer fills up fully)
-//     this.currentSource.onended = null;
-
-//     this.currentSource = this.audioContext.createBufferSource();
-//     this.currentSource.buffer = buffer;
-//     this.currentSource.connect(this.audioContext.destination);
-//     this.currentSource.playbackRate.value = playbackRate;
-//     //console.log("STARTING");
-//     this.currentSource.onended = () => { 
-//         //console.log("seconds elapsed: " + (this.audioContext.currentTime - this.soundStart)); 
-//         console.log("sound end: " + (this.audioContext.currentTime)); 
-
-//         this.soundFinished = true
-//         setTimeout(() => {
-//             this.playSound2();    
-//         });
-        
-//     };
-//     this.currentSource.start();
-
-//     this.soundFinished = false;
-//     this.soundStart = this.audioContext.currentTime;
-
-//     //reset the sample index cause we drained it
-//         //this.sampleIndex = 0;
-
-//     //source.stop(this.audioContext.currentTime + (this.sampleRate / this.sampleArrLength) / 2);
-
-//     // if (this.prevSource)
-//     //     this.prevSource.stop();
-//     // this.prevSource = source;
-//     // source.
-
-// }
 
 //executes numCycles, returns the number of cycles before the next "event" 
 sound.prototype.update = function(numCycles) {
     if (!this.enable)
         return this.cyclesPerSample;
-    // this.numCycle += numCycles;
 
-    // this.squareChannel1.update(numCycles);
-    // this.squareChannel2.update(numCycles);
-
-    // if (this.numCycle === this.cyclesPerSample) {
-    //     this.numCycle = 0;
-    //     this.sampleArr[this.sampleIndex] = this.getSample();
-
-    //     this.sampleIndex ++;
-    //     if (this.sampleIndex === this.sampleArrLength) {
-    //         this.sampleIndex = 0;
-    //         this.playSound();
-    //     }
-
-    //     //soundchannels.sample()
-
-    // }
-
-    // if (this.numCycle > this.cyclesPerSample)
-    //     throw new Error("wtf")
-    
-    // //this.numCycle %= this.cyclesPerSample;
-    // return this.cyclesPerSample - this.numCycle;
     this.numCycle += numCycles;
 
     this.squareChannel1.update(numCycles);
@@ -319,106 +108,27 @@ sound.prototype.update = function(numCycles) {
         this.sampleArr[this.sampleIndex] = this.getSample();
         this.sampleIndex ++;
 
-        //drain sound buffer
-        // if (this.sampleIndex === this.sampleArrLength || this.soundFinished)
-        //     this.playSound();
-
         //drain the buffer if it overflows or we are nearing the end of the samples being currently played
         if (this.sampleIndex === this.sampleArrLength 
-            || (this.soundStartTimeElapsed() / this.soundLength) > .85)
+            || (this.soundStartTimeElapsed() / this.soundLength) > this.pctSoundFinishedThreshold)
             this.playSound();
-        //else
-            //this.sampleIndex ++;
-
-
-        //soundchannels.sample()
-
     }
 
     if (this.numCycle > this.cyclesPerSample)
         throw new Error("wtf")
     
-    //this.numCycle %= this.cyclesPerSample;
     return this.cyclesPerSample - this.numCycle;
 };
 
-sound.prototype.start = function(enable) {
-    // if (enable) {
-        this.soundStart = this.audioContext.currentTime;
-        this.soundFinished = false;
-        //for each channel, start
-        this.squareChannel1.start();
-        this.squareChannel2.start();
-    // }
-    // else {
-    //     //for each channel, stop
-    // }
+sound.prototype.start = function() {
+    this.enable = true;
+    //init stuff
+    this.soundStart = this.audioContext.currentTime;
+    //for each channel, init
+    this.squareChannel1.init();
+    this.squareChannel2.init();
 }
 
-//1 frame passes
-//1 frame worth of samples
-//play the samples
-//after buffer drained (sound ends)
-//three outcomes
-//buffer is perfectly full (num samples matches the amount of time spent draining), play as normal
-//buffer overflow, there are more samples than there should be in the time spent draining -> playback rate increase
-//buffer underflow, there arent enough samples -> playback rate decrease
-
-//doing this, should normalize to perfectly full case
-
-//implementation
-//keep an extra large buffer for holding more samples
-//playsound condition is (prev sound finished, maybe init this with a jank settimeout) //and numSamples == ideal buffer length)
-//init soundfinished = true
-//on end, soundfinished = false (callback attached, how do we serialize this?? prob just default to set the flag to false on setstate, and let the sound get screwed up for a brief moment)
-//check how many samples are in the buffer while prev samples were being drained
-//if are too much, play em faster, flag gets set faster in the callback, so that next iteration is perfect buffer elength
-//too little, play em slower, flag gets set slower in the callback, so that next iteration is perfect buffer elength
-
-//in essence, if emulator running uber fast, sound is going to be sped up
-//in the other direction, sound is going to be slow
-//if fps is janky, this algo will tend towards ideal
-
-//playback rate can be modified to induce slow / fast
-
-//play after sound finished -> how do we initialize this?
-//play after sound finished + num cycles -> will result in gaps if sound finishes but num cycles hasnt finished, but initialization would work fine...
-//play after sound finished OR sound buffer full -> this fulfills both cases above!!!! on init, once sound buffer is full, play them (and just init sound drain time to max buff len)
-//on subsequent calls, unless cpu is running really fast, will hit the sound finished condition and flow into fast / slow cases above
-//if cpu is running really fast, we need to drain the buffers anyway cause we got no space, nothing we can do about it, in which case we play the sound resulting in sound overlap
-
-//init souund start in start fn to date.now
-//time elapsed = actual samples / (date.now - sound start).milliseconds * convert to numsamples = playback rate
-//sample rate * seconds = ideal samples
-
-
-//how do we handle sound overflow
-//because the play condition will hover around full buffer, overflow will happen very frequently, especaiilly if we reduce the length of the sound buffer (latency)
-//thus, we want to add a bit of leeway somehow
-
-//what if we make the sound play fast, to make the cpu slower relatively
-//this would mean less samples in the buffer,
-//it would calculate that 
-
-//sound plays
-//sound finishes
-//see if num samples expected vs actual
-//adjust playback rate
-//will tend towards initial buffer length in terms of sound length
-
-//what we want is for it to tend towards some smaller buf length, which allows for overflow
-//becaues if it tends towards initial buffer length, the playsound condition will be the buffer full condition, which means choppiness
-//instead of adjust playback rate to tend towards the length of the previous sound, have it tend towards a set length (say, 75% of the buffer)
-//implement some easing as well (25% of some static amount at a time, if less than that, just close the gap)
-//done
-
-//how do we remove the delay in the sound?
-//double buffering
-//we kind of already have a double buffering because after draining the buffer immediately starts filling up
-//if we drain 
-
-
-//nah, simple approach is to have the sounds overlap. need to tend the timing such that it is close to perfect, and then kill the previous sound
-//this ensures that when playing the next one, we have zero gaps
-//this WILL introduce artifacts if we cut off a big chunk from the previous sound, but a timing issue is much easier to solve than trying to thwart
-//javascripts single threaded nature (where we are totally at the mercy of the event loop which is causing our issues)
+sound.prototype.stop = function() {
+    this.enable = false;
+}
